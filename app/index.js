3f43b9ed10d04242113ecdaacf59dccfd099b5fa
oncomouse <oncomouse@gmail.com>
2018-01-31 12:22:06 -0600

    Automatically generate (and separetely bundle) polyfills

--- app/index.js
+++ app/index.js
@@ -3,6 +3,7 @@ import { withEffects } from 'hyperapp-effects'
 import actions from './actions'
 import state from './state'
 import App from './containers/App'
+import loadPolyfills from './utilities/loadPolyfills'
 
 const view = state => (
     <App
@@ -11,6 +12,9 @@ const view = state => (
 )
 
 const appifier = withEffects(app);
-const main = (process.env.NODE_ENV === 'production' ?
-    appifier
-    : require('@hyperapp/logger').default({})(appifier))(state, actions, view, document.body)
+let main
+loadPolyfills(() => {
+    main = (process.env.NODE_ENV === 'production' ?
+        appifier
+        : require('@hyperapp/logger').default({})(appifier))(state, actions, view, document.body)
+})
